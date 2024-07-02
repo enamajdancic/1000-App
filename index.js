cc2adf4df8629a24935d5191802e1913fa663031
enamajdancic <ena.majdancic1@gmail.com>
2024-07-02 13:58:28 +0200

    added index.js file

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
+
+
 function insert(){
+
+    
     let text = document.getElementById("text");
     let unos = document.getElementById("unos");
     let p = document.createElement("li");
 
     let inserted = unos.value;
-    
+
     
     p.textContent = inserted;
+    
     text.appendChild(p);
+
+    most= text.style.color ="red";
+    medium= text.style.color ="yellow";
+    least= text.style.color ="green"; 
+
+    let most = document.getElementById("most");
+    let medium = document.getElementById("medium");
+    let least = document.getElementById("least");
+    let importance = document.getElementByid("importance");
+
+
 }
-
-
